e641b72181b42bd1328f21dde42aea568934bc7d
SImran Tyagi <111629975+siimrann@users.noreply.github.com>
2023-09-24 14:15:51 +0530

    frontend login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,11 +3,14 @@ const AuthLayout = ({
 }: {
   children: React.ReactNode
 }) => {
-  return ( 
-    <div className="h-full flex items-center justify-center">
-      {children}
+  return (
+    <div className="h-full bg-gradient-to-r from-[#3F1D38] via-[#6A097D] to-[#4D3C77]  ">
+      <div className="h-full  flex items-center justify-center">
+
+        {children}
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default AuthLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
+import { dark } from '@clerk/themes';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider  appearance={{
+      baseTheme: dark
+    }}>
       <html lang="en">
         <body className={inter.className}>
           <ConfettiProvider />
